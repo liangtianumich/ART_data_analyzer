bdf027d032f91c0d4afc6bda835c192842674168
Liang Tian <liangtianisu@gmail.com>
2018-04-23 08:01:27 -0400

    customized local atomic strain calculation, need to incorporate pbc when determining the d_ji

--- src/python/calculator/strain_calculator.py
+++ src/python/calculator/strain_calculator.py
@@ -110,7 +110,7 @@ class Atom(object):
 	@classmethod
 	def from_ds(cls, data):
 		"""
-		create a Atomic_Coordinate object from pandas.Series
+		create a Atom class object from pandas.Series
 		Input argument:
 			data: instance of pandas.Series
 				data of a single atom
@@ -144,9 +144,161 @@ class Atom(object):
 		for i in unique_atom_type:
 			groups[i] = data.loc[[data["atom_id"] == i]]
 		return groups
+	
+	@classmethod
+	def to_list(cls, data):
+		"""
+		this method convert a class Atom object into a list whose three elements
+		are x,y,z
+		"""
+		if isinstance(data, Atom) == False:
+			raise Exception("data must be a class object")
+		data.atom_loc
+		return []
 
 
-def local_strain_calculator(initial_config_data, saddle_config_data):
+def local_strain_calculator(initial_config_data, saddle_config_data, cut_off_distance, atom_list = None, save_results = True):
+	"""
+	this function calculate various local atomic strain quantities for atoms whose item id stored inside
+	the atom_list 
+	input arguments:
+	
+	initial_config_data: instance of pandas.Dataframe
+		intial configuration data
+	
+	saddle_config_data: instance of pandas.Dataframe
+		saddle configuration data
+		
+	cut_off_distance: dict
+		dictionary contains the multiple pair cut-off distance
+		currently use tuples as keys for immutability
+		for example, {(1,1):3.7,(1,2):2.7,(2,2):3.0} means that atom_type 1 and 1 cut-off
+		is 3.7, etc
+		It is suggested that cut_off_distance are used as a user input,
+		not implemented by calculating from the first min of pair distribution function
+		of initial_config_data since it is not a module that reused a lot
+		it is good enough to get this single value through Ovide coordination analysis rdf or other software
+		instead of a rigourous calculation
+	
+	atom_list: list,
+		a list storing the atom item id of interested atoms
+		default calculating all atoms in the initial_config_data
+	
+	save_results: boolean, default True
+		if True, save calculated local strains results into a json file
+		
+	returns:
+		strain: dict()
+			a dictionary with key being the item id number of the interested atom
+			values being the calculated various local atomic strains measures 
+			stored inside in a pandas.Series
+			
+	Note:
+		the nearest neighbor is determined through the atomic configuration in
+		initial_config_data
+	
+	
+	"""
+	# if not specifying the atom_list, choose all atoms in the initial_config_data
+	if atom_list is None:
+		atom_list = (initial_config_data["item"]).tolist()
+	_data = initial_config_data
+	
+	# check if the nn results.json file exists or not
+	
+	nn = NN_finder_all(initial_config_data,cut_off_distance, atom_list)
+	
+	strain = dict()
+	
+	for item in atom_list:
+		
+		item_nn = nn[item]
+		NN_list = item_nn["item"].tolist()
+		NN_list.append(item)
+		
+		# NN_intial is the pandas.Dataframe of intial data with interested_atom and its NN
+		NN_intial = initial_config_data.loc[initial_config_data["item"].isin(NN_list)]
+		
+		# NN_saddle is the pandas.Dataframe of saddle data with interested_atom and its NN
+		NN_saddle = saddle_config_data.loc[saddle_config_data["item"].isin(NN_list)]
+		#NN_intial = initial_config_data.loc[(initial_config_data['item'].isin(item_nn["item"].tolist()))]
+		#NN_intial.append(item)
+		#NN_saddle = (saddle_config_data['item'].isin(item_nn["item"])).tolist() 
+		#saddle_config_nn = saddle_config_data.loc[(saddle_config_data['item'].isin(item_nn["item"])).tolist()]
+		
+		#local_strains should be a dict as well since it has multiple output strain
+		# or a pandas Series
+		local_strains = local_strain_calculator_atom(NN_initial, NN_saddle)
+		strain[item] = local_strains
+	
+	if save_results is True:
+		with open('strain_result.json', 'w') as f:
+			json.dump(strain,f)
+	return strain
+
+
+def local_strain_calculator_atom(initial_config_atom, saddle_config_atom, atom_item):
+	"""
+	this function calculate the local atomic strain according to the algorithm
+	proposed in http://li.mit.edu/Archive//Graphics/A/annotate_atomic_strain/Doc/main.pdf
+	
+	Input arguments:
+		
+		initial_config_atom: an instance of pandas.Dataframe
+			interested atom and its NN atoms in intial configuration data stores atom id, atom type and x,y,z coordinates of the dump file
+		
+		saddle_config_atom: an instance of pandas.Dataframe
+			interested atom and its NN atoms in saddle configuration data stores atom id, atom type and x,y,z coordinates of the dump file
+		
+		atom_item: integer
+			the item id of interested atom
+	return:
+		atom_strain: an instance of pandas.Series
+			storing various local atomic strains in pandas.Series
+			such as the von mises strain, 
+	"""
+	# calculate dij and d0_ij
+	NN_intial = initial_config_atom.loc[initial_config_atom['item'] != atom_item]
+	Atom_initial = initial_config_atom.loc[initial_config_atom['item'] == atom_item]
+	Atom_ini_obj = Atom.from_ds(Atom_initial)
+	
+	NN_saddle = saddle_config_atom.loc[saddle_config_atom['item'] != atom_item]
+	Atom_saddle = saddle_config_atom.loc[saddle_config_atom['item'] == atom_item]
+	Atom_sad_obj = Atom.from_ds(Atom_saddle)
+	
+	Dim = 3
+	V = np.zeros(shape=(Dim,Dim))
+	W = np.zeros(shape=(Dim,Dim))
+	
+	for (index,atom_ini_NN) in NN_initial.iterrows():
+		# d0_ji in pandas.Series
+		d0_ji = Atom.from_ds(atom_int_NN) - Atom_ini_obj
+		atom_sad_NN = Atom.from_ds(NN_sandle.loc[NN_saddle["item"] == atom_NN["item"]])
+		# d_ji in pandas.Series
+		d_ji = atom_sad_NN - Atom_sad_obj
+		# begin calculate the V and M matrix
+		V[0][0] = V[0][0] + d0_ji[0] * d0_ji[0]
+		V[0][1] = V[0][1] + d0_ji[0] * d0_ji[1]
+		V[0][2] = V[0][2] + d0_ji[0] * d0_ji[2]
+		V[1][0] = V[1][0] + d0_ji[1] * d0_ji[0]
+        V[1][1] = V[1][1] + d0_ji[1] * d0_ji[1]
+        V[1][2] = V[1][2] + d0_ji[1] * d0_ji[2]
+		V[2][0] = V[2][0] + d0_ji[2] * d0_ji[0]
+        V[2][1] = V[2][1] + d0_ji[2] * d0_ji[1]
+        V[2][2] = V[2][2] + d0_ji[2] * d0_ji[2]
+
+		W[0][0] = W[0][0] + d0_ji[0] * d_ji[0]
+		W[0][1] = W[0][1] + d0_ji[0] * d_ji[1]
+		W[0][2] = W[0][2] + d0_ji[0] * d_ji[2]
+		W[1][0] = W[1][0] + d0_ji[1] * d_ji[0]
+		W[1][1] = W[1][1] + d0_ji[1] * d_ji[1]
+		W[1][2] = W[1][2] + d0_ji[1] * d_ji[2]
+		W[2][0] = W[2][0] + d0_ji[2] * d_ji[0]
+		W[2][1] = W[2][1] + d0_ji[2] * d_ji[1]
+		W[2][2] = W[2][2] + d0_ji[2] * d_ji[2]
+		
+	d0 = initial_config_atom.
+	
 	
 
 def NN_finder_all(initial_config_data,cut_off_distance, atom_list = None, save_results = True):
@@ -364,23 +516,4 @@ def cut_off_distance_calculator(config_data):
 			cut_off_distance.append(first_min_pdf(pair_dist_fun))
 	return cut_off_distance
 
-def local_strain_calculator_atom(NN_data_initial, NN_data_saddle, atom_id):
-	"""
-	this function calculate the local atomic strain according to the algorithm
-	proposed in http://li.mit.edu/Archive//Graphics/A/annotate_atomic_strain/Doc/main.pdf
-	
-	Input arguments:
-		
-		initial_data: an instance of pandas.Dataframe
-			intial configuration stores atom id, atom type and x,y,z coordinates of the dump file
-		saddle_data: an instance of pandas.Dataframe
-			saddle configuration stores atom id, atom type and x,y,z coordinates of the dump file
 
-	return:
-		a list of pandas.Series instances
-		stored various local atomic strain in pandas.Series
-		such as the von mises strain, 
-	
-	explain the details of this funtion, what is the input and output, is the output \
-	strain matrix relative to a single atom or for all atoms  */
-	"""
