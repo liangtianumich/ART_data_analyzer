9849f0b1558b30999b0d69b44d341cb76766d07b
ltian <liangtianisu@gmail.com>
2018-05-08 18:23:58 -0500

    commit on my Ubuntu

--- environment.sh
+++ environment.sh
@@ -1,7 +1,7 @@
 # set up environmental variables before using this package
 
 # the directory path of the python package in user's machine
-export MY_ART=/Users/ltian/Documents/alabama/ART_data_analyzer
+export MY_ART=~/ART_data_analyzer
 
 # user need to specify the ART data directory
 export DATA_DIR=$MY_ART/examples/
